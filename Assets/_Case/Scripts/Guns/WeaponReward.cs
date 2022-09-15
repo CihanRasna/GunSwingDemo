e2162ce54d7de4135be8f6f5ba278f72a7c9a1d0
CihanRSN <cihanrasna@gmail.com>
2022-09-15 14:59:27 +0300

    Weapon reward object

---
+++ Assets/_Case/Scripts/Guns/WeaponReward.cs
@@ -0,0 +1,18 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace _Case.Scripts.Guns
+{
+    public class WeaponReward : MonoBehaviour
+    {
+        [SerializeField] private GameObject dummy;
+        
+        [SerializeField] private Weapon rewardPrefab;
+        public Weapon RewardPrefab => rewardPrefab;
+
+        private void Start()
+        {
+            dummy.transform.DORotate(new Vector3(0, 360, 0), 2f, RotateMode.WorldAxisAdd).SetLoops(-1).SetEase(Ease.Linear);
+        }
+    }
+}
